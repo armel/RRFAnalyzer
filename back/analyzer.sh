537af0ee50bfa77135f967a13b391339cd4ba7fa
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-04-24 04:23:32 +0200

    Add shell script

--- back/analyzer.sh
+++ back/analyzer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python /opt/RRFAnalyzer/back/analyzer.py --room ALL --day 1 --format JSON > /var/www/RRFAnalyser/analyzer_1.json
-python /opt/RRFAnalyzer/back/analyzer.py --room ALL --week 0 --format JSON > /var/www/RRFAnalyser/analyzer_7.json
-python /opt/RRFAnalyzer/back/analyzer.py --room ALL --format JSON > /var/www/RRFAnalyser/analyzer_30.json
-python /opt/RRFAnalyzer/back/analyzer.py --room ALL --day 90 --format JSON > /var/www/RRFAnalyser/analyzer_90.json
-python /opt/RRFAnalyzer/back/analyzer.py --room ALL --day 180 --format JSON > /var/www/RRFAnalyser/analyzer_180.json
+python /opt/RRFAnalyzer/back/analyzer.py --room ALL --day 1 --format JSON > /var/www/RRFAnalyzer/analyzer_1.json
+python /opt/RRFAnalyzer/back/analyzer.py --room ALL --week 0 --format JSON > /var/www/RRFAnalyzer/analyzer_7.json
+python /opt/RRFAnalyzer/back/analyzer.py --room ALL --format JSON > /var/www/RRFAnalyzer/analyzer_30.json
+python /opt/RRFAnalyzer/back/analyzer.py --room ALL --day 90 --format JSON > /var/www/RRFAnalyzer/analyzer_90.json
+python /opt/RRFAnalyzer/back/analyzer.py --room ALL --day 180 --format JSON > /var/www/RRFAnalyzer/analyzer_180.json
